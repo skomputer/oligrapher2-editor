743c15b795dddcea91374aeec8b8abd548cb6600
Matthew Skomarovsky <matthew@verypolite.com>
2015-11-13 16:23:36 -0500

    updated help screen

--- app/components/HelpScreen.jsx
+++ app/components/HelpScreen.jsx
@@ -6,22 +6,29 @@ export default class HelpScreen extends Component {
     return (
       <div id="helpScreen">
         <h3>User Guide</h3>
+
         Type a name in the "add node" box and press enter to add the node to the graph. 
-        { this.props.source ? ` Nodes from ${this.props.source.name} matching the name you type will appear below; click on them to add them to the graph; all edges connecting it to nodes already in the graph will be added too.` : "" }
+        { this.props.source ? ` Nodes from ${this.props.source.name} matching the name you type will appear below; click on them to add them to the graph.` : "" }
         <br />
+        <br />
+        <strong>Editing Nodes and Edges</strong><br />
+        <br />
+        CLICK a node or edge to select or deselect it.<br />
+        SHIFT+CLICK to select mutiple nodes and edges.<br />
+        <br />
+        Select a node or edge to view an editing form in the top-right corner of the map. Changes you make in the form will upate the node or edge immediately.<br />
         <br />
         <strong>Buttons</strong><br />
         <br />
-        The "circle" button arranges nodes in a circle.<br />
-        The "prune" button removes unconnected nodes.<br />
-        The "clear" button deletes all content from the graph.<br />
+        The CIRCLE button arranges nodes in a circle.<br />
+        The PRUNE button removes unconnected nodes.<br />
+        The  button deletes all content from the graph.<br />
         <br />
         <strong>Shortcut Keys</strong><br />
         <br />
-        ALT+CLICK a node or edge to select or deselect it.<br />
-        <br />
-        ALT+D deletes a selected node or edge<br />
-        ALT+E adds an edge.<br />
+        ALT+H toggles this user guide.<br />
+        ALT+D deletes selected nodes and edges.<br />
+        ALT+E adds an edge. Selected nodes will be auto-populated in the form.<br />
         ALT+C adds a caption.<br />
         <br />
         If ALT keys interfere with your browser or operating system shortcuts, all of the above shortcuts will work with CTRL instead of ALT<br />
@@ -30,9 +37,7 @@ export default class HelpScreen extends Component {
         CTRL+"-" zooms out.<br />
         CTRL+0 resets zoom.<br />
         <br />
-        <strong>Editing Nodes and Edges</strong><br />
-        <br />
-        Select a node or edge to view an editing form in the top-right corner of the map. Changes you make to the edit form will upate the node or edge immediately.<br />
+        ESC closes all forms.<br />
       </div>
     );
   }
